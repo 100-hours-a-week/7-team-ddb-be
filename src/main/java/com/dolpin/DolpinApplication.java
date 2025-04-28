bacecb57314bdac0f49a62c00eb50e308bd2ad14
ppongppang <ppongppangppung@gmail.com>
2025-04-28 19:38:03 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/dolpin/DolpinApplication.java
@@ -0,0 +1,13 @@
+package com.dolpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolpinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolpinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dolpin/DolpinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolpin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolpinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
