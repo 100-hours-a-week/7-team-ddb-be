57ae53e08b761ec46f5839a9e41f899d48d52148
ppongppang <ppongppangppung@gmail.com>
2025-04-29 16:13:44 +0900

    feat: User repository 추가

---
+++ src/main/java/com/dolpin/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dolpin.domain.user.repository;
+
+import com.dolpin.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
