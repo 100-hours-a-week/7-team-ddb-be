d7588f61149333bb37d66783efbb8b6ccd6c46ef
ppongppang <ppongppangppung@gmail.com>
2025-04-30 10:18:28 +0900

    feat: url 리다이렉트를 위한 dto/response

---
+++ src/main/java/com/dolpin/domain/auth/dto/response/OAuthUrlResponse.java
@@ -0,0 +1,12 @@
+package com.dolpin.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OAuthUrlResponse {
+    private String redirect_url;
+}
