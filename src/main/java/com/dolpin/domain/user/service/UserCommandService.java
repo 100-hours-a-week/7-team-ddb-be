8d9120277598918ad2c1872b2e63434109079d21
ppongppangppung <164026835+ppongppang@users.noreply.github.com>
2025-05-01 22:51:33 +0900

    Feat : 프로필 업데이트 API 구현

--- src/main/java/com/dolpin/domain/user/service/UserCommandService.java
+++ src/main/java/com/dolpin/domain/user/service/UserCommandService.java
@@ -5,7 +5,7 @@ import com.dolpin.domain.user.entity.User;
 
 public interface UserCommandService {
     User createUser(OAuthInfoResponse oAuthInfo);
-    void updateProfile(Long userId, String username, String imageUrl, String introduction);
+    User updateProfile(Long userId, String username, String imageUrl, String introduction);
     void agreeToTerms(Long userId, boolean isPrivacyAgreed, boolean isLocationAgreed);
     void deleteUser(Long userId);
     void registerUser(Long userId, String nickname, String profileImage, String introduction);
