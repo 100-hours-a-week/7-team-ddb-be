103dc3f14d6becbd51ece692cffbfec60db75bd3
ppongppang <ppongppangppung@gmail.com>
2025-05-01 11:11:29 +0900

    refactor : 인터페이스 단순화

--- src/main/java/com/dolpin/domain/auth/service/oauth/kakao/KakaoInfoResponse.java
+++ src/main/java/com/dolpin/domain/auth/service/oauth/kakao/KakaoInfoResponse.java
@@ -13,43 +13,10 @@ public class KakaoInfoResponse implements OAuthInfoResponse {
     @JsonProperty("id")
     private Long id;
 
-    @JsonProperty("kakao_account")
-    private KakaoAccount kakaoAccount;
-
-    @Getter
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    static class KakaoAccount {
-        private String email;
-        private Profile profile;
-
-        @Getter
-        @JsonIgnoreProperties(ignoreUnknown = true)
-        static class Profile {
-            private String nickname;
-
-            @JsonProperty("profile_image_url")
-            private String profileImageUrl;
-        }
-    }
 
     @Override
     public String getProviderId() {
         return id.toString();
-    }
-
-    @Override
-    public String getEmail() {
-        return kakaoAccount.email;
-    }
-
-    @Override
-    public String getNickname() {
-        return kakaoAccount.profile.nickname;
-    }
-
-    @Override
-    public String getProfileImageUrl() {
-        return kakaoAccount.profile.profileImageUrl;
     }
 
     @Override
