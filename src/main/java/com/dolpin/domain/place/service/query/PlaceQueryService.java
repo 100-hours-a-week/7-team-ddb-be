a363d22a01a33333c53edcd5b52c0f0ecc34a64d
ppongppang <ppongppangppung@gmail.com>
2025-05-04 20:25:39 +0900

    Feat : 장소 query 서비스 인터페이스에 장소 세부 정보 추가 및 경로 변경

---
+++ src/main/java/com/dolpin/domain/place/service/query/PlaceQueryService.java
@@ -0,0 +1,21 @@
+package com.dolpin.domain.place.service.query;
+
+import com.dolpin.domain.place.dto.response.PlaceAiResponse;
+import com.dolpin.domain.place.dto.response.PlaceDetailResponse;
+import com.dolpin.domain.place.dto.response.PlaceSearchResponse;
+import com.dolpin.domain.place.entity.Place;
+import com.dolpin.global.exception.BusinessException;
+import com.dolpin.global.response.ResponseStatus;
+import org.locationtech.jts.geom.Point;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public interface PlaceQueryService {
+
+    PlaceSearchResponse searchPlaces(String query, Double lat, Double lng, String category);
+
+    PlaceDetailResponse getPlaceDetail(Long placeId);
+}
+
