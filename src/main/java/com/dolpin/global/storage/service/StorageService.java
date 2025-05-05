8c8cef0b2499ba24ebfa9cea0608f00356f4a112
ppongppang <ppongppangppung@gmail.com>
2025-05-05 15:35:06 +0900

    Feat : presigned url API service 인터페이스 및 구현체 작성

---
+++ src/main/java/com/dolpin/global/storage/service/StorageService.java
@@ -0,0 +1,10 @@
+package com.dolpin.global.storage.service;
+
+import com.dolpin.global.storage.dto.request.PresignedUrlRequest;
+import com.dolpin.global.storage.dto.reseponse.PresignedUrlResponse;
+
+public interface StorageService {
+    PresignedUrlResponse generateSignedUrl(PresignedUrlRequest request);
+    String getFileUrl(String path);
+    void deleteFile(String path);
+}
