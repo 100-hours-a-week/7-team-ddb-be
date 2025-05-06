d055b4f0fbf40fd5c78c95a6ef050f8181ae2424
ppongppang <ppongppangppung@gmail.com>
2025-05-06 14:39:20 +0900

    refactor : 입력 값 검증 및 예외 처리

--- src/main/java/com/dolpin/global/response/ResponseStatus.java
+++ src/main/java/com/dolpin/global/response/ResponseStatus.java
@@ -22,7 +22,10 @@ public enum ResponseStatus {
 
     // 사용자
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다." );
+    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다." ),
+
+    // 장소
+    PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "장소를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private String message;
