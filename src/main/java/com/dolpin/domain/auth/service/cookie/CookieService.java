a9f05f24f1d9fa6e7d643e9887d808d9baa1edef
ppongppangppung <164026835+ppongppang@users.noreply.github.com>
2025-05-08 10:23:21 +0900

    Refactor : access token 방식 변경
    * Refactor : cookie service 추가 메서드 분리
    
    * Refactor : cookie service 주입하여 엑세스 토큰도 http-only 사용

---
+++ src/main/java/com/dolpin/domain/auth/service/cookie/CookieService.java
@@ -0,0 +1,47 @@
+package com.dolpin.domain.auth.service.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieService {
+
+    public void addAccessTokenCookie(HttpServletResponse response, String accessToken, long expiresIn) {
+        Cookie cookie = new Cookie("access_token", accessToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge((int) expiresIn);
+        cookie.setAttribute("SameSite", "Lax");
+        response.addCookie(cookie);
+    }
+
+    public void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
+        Cookie cookie = new Cookie("refresh_token", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(14 * 24 * 60 * 60);  // 14일 유효
+        cookie.setAttribute("SameSite", "Lax");
+        response.addCookie(cookie);
+    }
+
+    public void deleteAccessTokenCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("access_token", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    public void deleteRefreshTokenCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("refresh_token", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+}
