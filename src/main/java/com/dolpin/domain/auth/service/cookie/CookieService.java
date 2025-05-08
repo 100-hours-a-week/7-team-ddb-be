f5e554c177636247da172c05af1b42a459b6a732
ppongppang <ppongppangppung@gmail.com>
2025-05-08 16:56:10 +0900

    Fix : cookieservice setSecure 변경

--- src/main/java/com/dolpin/domain/auth/service/cookie/CookieService.java
+++ src/main/java/com/dolpin/domain/auth/service/cookie/CookieService.java
@@ -10,7 +10,7 @@ public class CookieService {
     public void addAccessTokenCookie(HttpServletResponse response, String accessToken, long expiresIn) {
         Cookie cookie = new Cookie("access_token", accessToken);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge((int) expiresIn);
         cookie.setAttribute("SameSite", "None");
@@ -20,7 +20,7 @@ public class CookieService {
     public void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
         Cookie cookie = new Cookie("refresh_token", refreshToken);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(14 * 24 * 60 * 60);  // 14일 유효
         cookie.setAttribute("SameSite", "None");
@@ -30,7 +30,7 @@ public class CookieService {
     public void deleteAccessTokenCookie(HttpServletResponse response) {
         Cookie cookie = new Cookie("access_token", null);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
@@ -39,7 +39,7 @@ public class CookieService {
     public void deleteRefreshTokenCookie(HttpServletResponse response) {
         Cookie cookie = new Cookie("refresh_token", null);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
