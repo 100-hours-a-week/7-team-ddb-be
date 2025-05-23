d45ea940ca2ccec3022269f6a7eff99e5ffe9f07
ppongppang <ppongppangppung@gmail.com>
2025-05-23 13:19:23 +0900

    Feat : webconfig에 인터셉터 설정 추가

--- src/main/java/com/dolpin/global/config/WebConfig.java
+++ src/main/java/com/dolpin/global/config/WebConfig.java
@@ -1,14 +1,28 @@
 package com.dolpin.global.config;
 
+import com.dolpin.global.interceptor.PerformanceLoggingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final PerformanceLoggingInterceptor performanceLoggingInterceptor;
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(performanceLoggingInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/v1/health", "/actuator/**");
     }
 }
