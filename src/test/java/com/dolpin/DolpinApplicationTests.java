5f57d7c34292641a92d9d42e193f114379e1ce10
ppongppang <ppongppangppung@gmail.com>
2025-05-27 20:50:58 +0900

    Feat : TestConfig import

--- src/test/java/com/dolpin/DolpinApplicationTests.java
+++ src/test/java/com/dolpin/DolpinApplicationTests.java
@@ -1,5 +1,6 @@
 package com.dolpin;
 
+import com.dolpin.global.config.TestConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
