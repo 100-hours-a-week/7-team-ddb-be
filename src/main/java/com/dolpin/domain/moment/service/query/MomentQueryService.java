4ede5d43d7c3e76ae85039231e61bd297229bcdb
ppongppang <ppongppangppung@gmail.com>
2025-06-03 20:03:37 +0900

    Feat: 기록 조회 서비스 인터페이스 추가

---
+++ src/main/java/com/dolpin/domain/moment/service/query/MomentQueryService.java
@@ -0,0 +1,18 @@
+package com.dolpin.domain.moment.service.query;
+
+import com.dolpin.domain.moment.dto.response.MomentDetailResponse;
+import com.dolpin.domain.moment.dto.response.MomentListResponse;
+import com.dolpin.domain.moment.dto.response.PlaceMomentListResponse;
+
+public interface MomentQueryService {
+
+    MomentListResponse getAllMoments(Integer limit, String cursor);
+
+    MomentListResponse getMyMoments(Long userId, Integer limit, String cursor);
+
+    MomentListResponse getUserMoments(Long targetUserId, Integer limit, String cursor);
+
+    PlaceMomentListResponse getPlaceMoments(Long placeId);
+
+    MomentDetailResponse getMomentDetail(Long momentId, Long currentUserId);
+}
