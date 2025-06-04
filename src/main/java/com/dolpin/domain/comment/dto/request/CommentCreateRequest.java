ef0253df3c42ccbe7638f6826b7ec0924caf4bcf
zinu <164026835+ppongppang@users.noreply.github.com>
2025-06-04 20:28:03 +0900

    Feat: 댓글 도메인 DTO 추가

---
+++ src/main/java/com/dolpin/domain/comment/dto/request/CommentCreateRequest.java
@@ -0,0 +1,17 @@
+package com.dolpin.domain.comment.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentCreateRequest {
+
+    @NotBlank(message = "댓글 내용은 필수입니다")
+    @Size(max = 1000, message = "댓글은 1000자 이내여야 합니다")
+    private String content;
+}
