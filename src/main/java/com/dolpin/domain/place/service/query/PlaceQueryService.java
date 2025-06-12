277c3f48340b3ef0e0f8e0203e50a097a0bb2864
ppongppang <ppongppangppung@gmail.com>
2025-06-12 15:08:44 +0900

    Feat: 장소 조회 서비스에 사용자별 북마크 정보 제공 기능 추가

--- src/main/java/com/dolpin/domain/place/service/query/PlaceQueryService.java
+++ src/main/java/com/dolpin/domain/place/service/query/PlaceQueryService.java
@@ -1,25 +1,18 @@
 package com.dolpin.domain.place.service.query;
 
-import com.dolpin.domain.place.dto.response.PlaceAiResponse;
 import com.dolpin.domain.place.dto.response.PlaceCategoryResponse;
 import com.dolpin.domain.place.dto.response.PlaceDetailResponse;
 import com.dolpin.domain.place.dto.response.PlaceSearchResponse;
-import com.dolpin.domain.place.entity.Place;
-import com.dolpin.global.exception.BusinessException;
-import com.dolpin.global.response.ResponseStatus;
-import org.locationtech.jts.geom.Point;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 public interface PlaceQueryService {
 
-    PlaceSearchResponse searchPlaces(String query, Double lat, Double lng, String category);
+    PlaceSearchResponse searchPlaces(String query, Double lat, Double lng, String category, Long userId);
 
-    PlaceDetailResponse getPlaceDetail(Long placeId);
+    PlaceDetailResponse getPlaceDetail(Long placeId, Long userId);
+
+    PlaceDetailResponse getPlaceDetailWithoutBookmark(Long placeId);
 
     PlaceCategoryResponse getAllCategories();
 
-    PlaceSearchResponse searchPlacesWithDevToken(String query, Double lat, Double lng, String category, String devToken);
+    PlaceSearchResponse searchPlacesWithDevToken(String query, Double lat, Double lng, String category, String devToken, Long userId);
 }
