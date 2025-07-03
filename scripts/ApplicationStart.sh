c3ac04322d044f1bf3e884c1fa6f18cc119e9b7e
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-07-03 13:25:44 +0900

    Feat : Redis 환경변수 반영

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -13,7 +13,7 @@ SECRET_JSON=$(aws secretsmanager get-secret-value \
 
 echo "$SECRET_JSON" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /home/ubuntu/.env
 
-echo "▶ DB 비밀번호 시크릿 조회 및 병합"
+echo "▶ DB, Redis 비밀번호 시크릿 조회 및 병합"
 cp /home/ubuntu/.env /home/ubuntu/.env.final
 
 SECRET_JSON=$(aws secretsmanager get-secret-value \
@@ -24,11 +24,27 @@ SECRET_JSON=$(aws secretsmanager get-secret-value \
 DB_PASSWORD=$(echo "$SECRET_JSON" | jq -r .password)
 DB_HOST=$(echo "$SECRET_JSON" | jq -r .host)
 
-echo "DB_PASSWORD=$DB_PASSWORD" >> /home/ubuntu/.env.final
-echo "DATASOURCE_URL=$DB_HOST" >> /home/ubuntu/.env.final
+SECRET_JSON=$(aws secretsmanager get-secret-value \
+  --secret-id ${ENV_NAME}/redis/credentials/secret \
+  --query SecretString \
+  --output text)
+
+REDIS_HOST=$(echo "$SECRET_JSON" | jq -r .host)
+REDIS_PORT=$(echo "$SECRET_JSON" | jq -r .port)
+REDIS_PASSWORD=$(echo "$SECRET_JSON" | jq -r .password)
+REDIS_USERNAME=$(echo "$SECRET_JSON" | jq -r .username)
+
+{
+  echo "DB_PASSWORD=$DB_PASSWORD"
+  echo "DATASOURCE_URL=$DB_HOST"
+  echo "REDIS_HOST=$REDIS_HOST"
+  echo "REDIS_PORT=$REDIS_PORT"
+  echo "REDIS_PASSWORD=$REDIS_PASSWORD"
+  echo "REDIS_USERNAME=$REDIS_USERNAME"
+} >> /home/ubuntu/.env.final
 
 docker run -d --name "$CONTAINER_NAME" \
-  --restart=always \
+  --restart=unless-stopped \
   --env-file /home/ubuntu/.env.final \
   -v /home/ubuntu/logs:/logs \
   -p "$PORT:$PORT" -p 8081:8081 \
