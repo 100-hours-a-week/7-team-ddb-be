e307a584f54f72f3a22b2a07ac7c6ca6583a87e3
ppongppang <ppongppangppung@gmail.com>
2025-07-17 17:56:16 +0900

    Feat : spring batch config 추가

---
+++ src/main/java/com/dolpin/global/config/BatchConfiguration.java
@@ -0,0 +1,24 @@
+package com.dolpin.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class BatchConfiguration {
+
+    @Bean
+    public JobLauncher asyncJobLauncher(JobRepository jobRepository) {
+        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return jobLauncher;
+    }
+}
